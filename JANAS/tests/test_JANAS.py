0b6864b96e9a8e9fbc171d38cd455f21e885a7ac
samyu-vakkalanka <spv15@georgetown.edu>
2024-12-14 20:33:13 -0700

    fix issues in main

--- JANAS/tests/test_JANAS.py
+++ JANAS/tests/test_JANAS.py
@@ -1,5 +1,20 @@
-from janas.src.janas.scrape_jp.scrape_jp import get_articles as get_japanese_articles
-from janas.src.janas.scrape_am.scrape_am_articles import get_am_articles as get_american_articles
-from janas.src.janas.sentiment_scoring.sentiment import analyze_sentiment
-from janas.src.janas.translation.translation import translate_articles
-from janas.src.janas.analysis.summarize import make_graphs as graph_generate
+from janas.src.janas.scrape_am.scrape_am_articles import fetch_and_save_articles
+# "test_am_scrape, test_nhk_scrape, test_s_scrape, test_m_scrape, test_translate (check if all characters changed from japanese characters), test_sentiment_am (see if scores are produced for all 4 types), test_sentiment_ja"
+
+
+def test_am_scrape():
+    data = fetch_and_save_articles(
+        {"Politics": "https://www.cnn.com/politics"}, "CNN", "Left", 1
+    )
+    assert len(data["link"]) != 0, "Link scrapping failed for article"
+    assert len(data["content"]) != 0, "Article scrapping failed"
+
+
+# def test_nhk_scrape():
+#     pass
+
+# def test_san_scrape():
+#     pass
+
+# def test_mai_scrape():
+#     pass
